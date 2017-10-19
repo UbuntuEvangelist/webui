8992981092734e2c437d6266a687137443d48af8
Henrikas Elsbergas <elsbergass@gmail.com>
2017-10-18 17:53:40 -0700

    Adding delete feature to expressions admin

--- client/modules/expressions/admin/views/expressions.js
+++ client/modules/expressions/admin/views/expressions.js
@@ -1,53 +1,71 @@
-define(["application", "./expression", './templates/expressions.tpl'],
-    function (App, expressionView, template) {
+define(['application', './expression', './templates/expressions.tpl'],
+    function(App, expressionView, template) {
         return Marionette.CompositeView.extend({
             template: template,
             childViewContainer: '.app-expressions',
             childView: expressionView,
             ui: {
-                nameField: '#app-expression-name',
+                nameField: '.app-expression-name',
                 addButton: '.app-create-expression',
+                deleteButton: '.app-delete-expression',
                 saveButton: '.app-save-expressions'
             },
             events: {
                 'keyup @ui.nameField': 'changeExpressionsName',
                 'click @ui.addButton': 'addExpressions',
-                'click @ui.saveButton': 'updateExpressions'
+                'click @ui.saveButton': 'updateExpressions',
+                'click @ui.deleteButton': 'deleteCurrent'
             },
-            initialize: function (options) {
-                this.mergeOptions(options, ['motors']);
+            initialize: function(options) {
+                this.mergeOptions(options, ['motors'])
                 this.childViewOptions = {
                     collection: this.motors,
                     expressionsView: this
-                };
-            },
-            addExpressions: function () {
-                var expression = new Backbone.Model({
-                    name: 'NewExpression',
-                    motor_positions: this.motors.getRelativePositions()
-                });
-                this.collection.add(expression);
-            },
-            updateExpressions: function () {
-                var self = this;
-
-                this.collection.sync(function () {
-                    App.Utilities.showPopover(self.ui.saveButton, 'Saved')
-                }, function () {
-                    App.Utilities.showPopover(self.ui.saveButton, 'Error saving expressions')
-                });
-            },
-            expressionButtonClicked: function (view) {
-                App.trigger('motors:selection:set', true);
-
-                if (this.last_clicked != view) {
-                    this.last_clicked = view;
-                    this.ui.nameField.val(view.model.get('name'));
                 }
             },
-            changeExpressionsName: function () {
-                if (typeof this.last_clicked != 'undefined')
-                    this.last_clicked.model.set('name', this.ui.nameField.val());
+            onAttach: function() {
+                this.ui.deleteButton.attr('disabled', true)
+            },
+            addExpressions: function() {
+                let expression = new Backbone.Model({
+                    name: 'NewExpression',
+                    motor_positions: this.motors.getRelativePositions()
+                })
+                this.collection.add(expression)
+            },
+            updateExpressions: function() {
+                let self = this
+
+                this.collection.sync(function() {
+                    App.Utilities.showPopover(self.ui.saveButton, 'Saved')
+                }, function() {
+                    App.Utilities.showPopover(self.ui.saveButton, 'Error saving expressions')
+                })
+            },
+            expressionButtonClicked: function(view) {
+                App.trigger('motors:selection:set', true)
+
+                if (this.last_clicked !== view) {
+                    this.last_clicked = view
+                    this.ui.nameField.val(view.model.get('name'))
+                    this.ui.deleteButton.attr('disabled', false)
+                }
+            },
+            changeExpressionsName: function() {
+                if (typeof this.last_clicked !== 'undefined')
+                    this.last_clicked.model.set('name', this.ui.nameField.val())
+            },
+            deleteCurrent: function() {
+                if (this.last_clicked) {
+                    this.last_clicked.model.destroy()
+                    this.updateExpressions()
+                    this.deselect()
+                }
+            },
+            deselect: function() {
+                this.last_clicked = null
+                this.ui.deleteButton.attr('disabled', true)
+                this.ui.nameField.val('')
             }
-        });
-    });
+        })
+    })
--- client/modules/expressions/admin/views/templates/expressions.tpl
+++ client/modules/expressions/admin/views/templates/expressions.tpl
@@ -1,16 +1,19 @@
 <div class="row">
     <div class="col-md-12">
-        <div class="app-expression-name form-group">
-            <label class="sr-only" for="app-expression-name">Amount (in dollars)</label>
+        <div class="app-expression-name-wrapper form-group">
             <div class="input-group">
-                <input type="text" class="form-control" id="app-expression-name" placeholder="Expression name">
+                <input type="text" placeholder="Expression name" class="app-expression-name form-control">
+                <span class="input-group-btn">
+                    <button class="app-delete-expression btn btn-danger" type="button">Delete</button>
+               </span>
             </div>
         </div>
 
         <div class="app-expressions expression-buttons"></div>
 
         <div class="input-group">
-            <button type="button" title="Add new expression" class="app-create-expression btn btn-default"><span class="glyphicon glyphicon-plus"></span></button>
+            <button type="button" title="Add new expression" class="app-create-expression btn btn-default"><span
+                    class="glyphicon glyphicon-plus"></span></button>
             <button type="button" class="app-save-expressions btn btn-primary">Save</button>
         </div>
     </div>
